901bf8c04af0351e17343464b38748386e2812aa
Jan Wichanski <jan.wichanski@magnetforensics.com>
2016-06-23 18:13:14 -0400

    No longer use activity for maps, load it through calls now

--- app/src/main/java/hackingthings/magnetathon/HomeScreen.java
+++ app/src/main/java/hackingthings/magnetathon/HomeScreen.java
@@ -1,17 +1,46 @@
 package hackingthings.magnetathon;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
 public class HomeScreen extends AppCompatActivity {
+
+    private static final String[] LOCATION_PERMS={
+            Manifest.permission.ACCESS_FINE_LOCATION,
+    };
+    private static final int INITIAL_REQUEST=333;
+    private static final int LOCATION_REQUEST=INITIAL_REQUEST+3;
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        try {
+            if (googleMap == null) {
+                googleMap = ((MapFragment) getFragmentManager().
+                        findFragmentById(R.id.map)).getMap();
+
+                if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                {
+                    requestPermissions(LOCATION_PERMS, LOCATION_REQUEST);
+                }
+
+                googleMap.setMyLocationEnabled(true);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
