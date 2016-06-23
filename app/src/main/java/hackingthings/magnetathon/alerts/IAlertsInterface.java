a9399a97065efd825733f7c9bb51189f727cc00f
Michelle MacKenzie <michelle.mackenzie@magnetforensics.com>
2016-06-23 10:48:01 -0400

    basic skeleton architecture

---
+++ app/src/main/java/hackingthings/magnetathon/alerts/IAlertsInterface.java
@@ -0,0 +1,11 @@
+package hackingthings.magnetathon.alerts;
+
+/**
+ * Created by mmackenzie on 6/23/2016.
+ */
+public interface IAlertsInterface {
+
+    public boolean SendSoftAlert();
+
+    public boolean SendHardAlert();
+}
---
+++ app/src/main/java/hackingthings/magnetathon/hardware/IHardwareInterface.java
@@ -0,0 +1,9 @@
+package hackingthings.magnetathon.hardware;
+
+/**
+ * Created by mmackenzie on 6/23/2016.
+ */
+public interface IHardwareInterface {
+
+    // may not need this
+}
