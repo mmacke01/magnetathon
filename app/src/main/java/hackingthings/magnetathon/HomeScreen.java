97803e600696f45d8880ec29678fb60914d0ee9f
Jan Wichanski <jan.wichanski@magnetforensics.com>
2016-06-24 05:53:09 -0400

    OMG...CONTACTS...NOW...:)

--- app/src/main/java/hackingthings/magnetathon/HomeScreen.java
+++ app/src/main/java/hackingthings/magnetathon/HomeScreen.java
@@ -1,23 +1,44 @@
 package hackingthings.magnetathon;
 
 import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 
-public class HomeScreen extends AppCompatActivity {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class HomeScreen extends AppCompatActivity
+{
     private static final String[] LOCATION_PERMS={
             Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+    private static final String[] CONTACTS_PERMS = {
+            Manifest.permission.READ_CONTACTS
     };
     private static final int INITIAL_REQUEST=333;
-    private static final int LOCATION_REQUEST=INITIAL_REQUEST+3;
+    private static final int REQUEST =INITIAL_REQUEST+3;
     private GoogleMap googleMap;
 
     @Override
@@ -27,12 +48,12 @@ public class HomeScreen extends AppCompatActivity {
 
         try {
             if (googleMap == null) {
-                googleMap = ((MapFragment) getFragmentManager().
-                        findFragmentById(R.id.map)).getMap();
+                googleMap = ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
 
-                if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                        checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
                 {
-                    requestPermissions(LOCATION_PERMS, LOCATION_REQUEST);
+                    requestPermissions(LOCATION_PERMS, REQUEST);
                 }
 
                 googleMap.setMyLocationEnabled(true);
@@ -40,6 +61,11 @@ public class HomeScreen extends AppCompatActivity {
         }
         catch (Exception e) {
             e.printStackTrace();
+        }
+
+        if (checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)
+        {
+            requestPermissions(CONTACTS_PERMS, REQUEST + 1);
         }
     }
     @Override
--- app/src/main/java/hackingthings/magnetathon/softContact.java
+++ app/src/main/java/hackingthings/magnetathon/softContact.java
@@ -46,7 +46,7 @@ public class softContact extends AppCompatActivity {
     }
 
     public void contactsClick(View view){
-        Intent intent = new Intent(this,ContactSelecter.class);
+        Intent intent = new Intent(this, Contacts.class);
         startActivity(intent);
     }
 
